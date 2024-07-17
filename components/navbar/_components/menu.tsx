92a8314f1edc2d620f1bd3eedeeb458e637aa536
mendsalbert <mendsalbert@gmail.com>
2024-07-17 20:22:18 +0000

    Subject: Remove contact form, job features & content changes
    Body:
    The previous contact form handling via email has been removed, promoting less reliance on direct email. Changes also include the removal of all operations related to jobs listing on the page, along with the Jobs link within the navigation menu, which should enhance navigation clarity. This alteration aims at streamlining user interactions and improving the focus on the main website service. No direct user-facing functionalities should be affected in any substantial way by these changes.
    
    No associated issue references.

--- components/navbar/_components/menu.tsx
+++ components/navbar/_components/menu.tsx
@@ -7,58 +7,11 @@ import { cn } from "@/lib/utils";
 
 import {
   NavigationMenu,
-  NavigationMenuContent,
   NavigationMenuItem,
   NavigationMenuLink,
   NavigationMenuList,
-  NavigationMenuTrigger,
   navigationMenuTriggerStyle,
 } from "@/components/ui/navigation-menu";
-
-import {
-  PiBookOpenTextLight,
-  PiFileThin,
-  PiSparkleLight,
-  PiTargetLight,
-} from "react-icons/pi";
-
-const components: { title: string; href: string; description: string }[] = [
-  {
-    title: "Alert Dialog",
-    href: "/docs/primitives/alert-dialog",
-    description:
-      "A modal dialog that interrupts the user with important content and expects a response.",
-  },
-  {
-    title: "Hover Card",
-    href: "/docs/primitives/hover-card",
-    description:
-      "For sighted users to preview content available behind a link.",
-  },
-  {
-    title: "Progress",
-    href: "/docs/primitives/progress",
-    description:
-      "Displays an indicator showing the completion progress of a task, typically displayed as a progress bar.",
-  },
-  {
-    title: "Scroll-area",
-    href: "/docs/primitives/scroll-area",
-    description: "Visually or semantically separates content.",
-  },
-  {
-    title: "Tabs",
-    href: "/docs/primitives/tabs",
-    description:
-      "A set of layered sections of content—known as tab panels—that are displayed one at a time.",
-  },
-  {
-    title: "Tooltip",
-    href: "/docs/primitives/tooltip",
-    description:
-      "A popup that displays information related to an element when the element receives keyboard focus or the mouse hovers over it.",
-  },
-];
 
 export function Menu() {
   return (
@@ -68,13 +21,6 @@ export function Menu() {
           <Link href="/" legacyBehavior passHref>
             <NavigationMenuLink className={navigationMenuTriggerStyle()}>
               Home
-            </NavigationMenuLink>
-          </Link>
-        </NavigationMenuItem>
-        <NavigationMenuItem>
-          <Link href="/jobs" legacyBehavior passHref>
-            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-              Jobs
             </NavigationMenuLink>
           </Link>
         </NavigationMenuItem>
